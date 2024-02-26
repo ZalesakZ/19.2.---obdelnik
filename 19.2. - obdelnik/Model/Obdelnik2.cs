deeab69c43b121e0d1d1f0eb3c72370ea614baf0
Zdeněk Zálešák <zalesakz@office365.zcu.cz>
2024-02-26 17:52:56 +0100

    Obdelník s Gettery a Settery

---
+++ 19.2. - obdelnik/Model/Obdelnik2.cs
@@ -0,0 +1,40 @@
+﻿namespace Model
+{
+    public class Obdelnik2
+{
+        private int stranaA;
+        private int stranaB;
+
+        public int StranaA
+        {
+            get => stranaA; 
+            set
+            {
+                if (stranaA != value)
+                {
+                    if(value < 0)
+                        stranaA = 0;    
+                    else
+                        stranaA = value;    
+                }
+            }
+        }
+        public int StranaB
+        {
+            get => stranaB; 
+            set
+            {
+                if (stranaB != value)
+                {
+                    if (value < 0)
+                        stranaB = 0;
+                    else
+                        stranaB = value;
+                }
+            }
+        }
+        public int Obvod => 2 * (stranaA + stranaB);   
+        public int Obsah => stranaA * stranaB; 
+        public double Uhlopricka => Math.Sqrt(StranaA * StranaA + Math.Pow(StranaB, 2));
+    }
+}
